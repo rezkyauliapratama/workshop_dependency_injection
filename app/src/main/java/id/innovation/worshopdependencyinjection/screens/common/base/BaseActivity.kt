060f87bf3bfc3a873ad4b9085829963ae02d3603
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-06-29 10:42:06 +0700

    create presentation composition root

--- app/src/main/java/id/innovation/worshopdependencyinjection/screens/common/base/BaseActivity.kt
+++ app/src/main/java/id/innovation/worshopdependencyinjection/screens/common/base/BaseActivity.kt
@@ -3,10 +3,22 @@ package id.innovation.worshopdependencyinjection.screens.common.base
 import androidx.appcompat.app.AppCompatActivity
 import id.innovation.worshopdependencyinjection.App
 import id.innovation.worshopdependencyinjection.common.dependencyinjection.CompositionRoot
+import id.innovation.worshopdependencyinjection.common.dependencyinjection.PresentationCompositionRoot
 
 abstract class BaseActivity: AppCompatActivity() {
 
-    protected fun getCompositionRoot(): CompositionRoot {
+    private lateinit var mPresentationCompositionRoot: PresentationCompositionRoot
+
+    protected fun getCompositionRoot(): PresentationCompositionRoot {
+        if (!::mPresentationCompositionRoot.isInitialized){
+            mPresentationCompositionRoot = PresentationCompositionRoot(
+                getAppCompositionRoot(),
+                supportFragmentManager
+            )
+        }
+        return mPresentationCompositionRoot
+    }
+    private fun getAppCompositionRoot(): CompositionRoot {
         return (application as App).getCompositionRoot()
     }
 }
